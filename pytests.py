854d8d44254a3f6a9cda063ea908d267318d5d38
Verantina007 <105320531+Verantina007@users.noreply.github.com>
2022-06-22 10:35:31 +0300

    Update pytests.py

--- pytests.py
+++ pytests.py
@@ -12,12 +12,12 @@ unittest.main(
 
 
 class TestStringMethods(unittest.TestCase):
-    #def test_upper(self):
-        #self.assertEqual('foo'.upper(), 'FOO')
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
    def test_isupper(self):
         self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        #self.assertFalse('Foo'.isupper())
 
     def test_split(self):
         s = 'hello world'
